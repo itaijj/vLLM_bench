44def317fbd89b36be075961027ae35431eb064c
itaijj <itaijj2@gmail.com>
2025-09-20 20:47:42 +0300

    small fix
    Signed-off-by: itaijj <itaijj2@gmail.com>

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -4,7 +4,7 @@ for variable in 1 5 10 15 25 30 35 40 45 50 55 100
 do
   # Commands to execute for each item
   echo "Running benchmark with  GUIDELLM__MAX_CONCURRENCY = $variable"
-   export GUIDELLM__MAX_CONCURRENCY=40; guidellm benchmark \
+   export GUIDELLM__MAX_CONCURRENCY=$variable; guidellm benchmark \
   --target "http://localhost:8000" \
   --rate-type throughput \
   --max-requests 100 \
